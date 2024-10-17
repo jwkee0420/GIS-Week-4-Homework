8c6e6e9c520fa907f33a5e87fc3536fe0c6368dd
jwkee0420 <keejinwen2000@gmail.com>
2024-10-17 12:22:47 +0100

    commit 2

--- script.R
+++ script.R
@@ -3,6 +3,7 @@ library(raster)
 library(terra)
 library(tidyverse)
 library(sf)
+library(tmap)
 
 csv <- read.csv('2010_2.csv')
 csv <- select(csv,c('countryIsoCode','country','value'))
@@ -11,3 +12,12 @@ csv2 <- select(csv2,c('countryIsoCode','country','value'))
 join <- csv %>% left_join(csv2, by=c('country'))
 
 difference <- join %>% mutate(difference = join$value.y - join$value.x)
+difference2 <- select(difference, c('countryIsoCode.y','difference'))
+
+world2 <- world %>% merge(., difference2, by.x= "color_code", by.y= "countryIsoCode.y")
+tmap_mode("plot")
+
+world2 %>%
+  qtm(.,fill = "difference")
+
+
